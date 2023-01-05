d47be3ab398c01f8accab2d4f5dcaf87e496b1c0
Umit-Ulusoy <88715653+Umit-Ulusoy@users.noreply.github.com>
2023-01-05 16:11:46 +0300

    add a part of filter course feature
    Added category filter feature into "courseController" file

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,9 +1,14 @@
 const Course = require('../models/Course');
+const Category = require('../models/Category');
 
 exports.createCourse = async (req, res) => {
 
     try {
-        const course = await Course.create(req.body);
+        const course = await Course.create({
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category
+        });
         res.status(201).json({
             status: 'success',
             course
@@ -19,9 +24,24 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) =>
 {
     try{
-        const courses = await Course.find();
+        const categorySlug = req.query.categories;
+
+        let filter = {};
+
+const category = await Category.findOne({ slug: categorySlug });
+
+if (categorySlug)
+{
+    filter = { category: category._id };
+}
+
+        const courses = await Course.find(filter);
+
+const categories = await Category.find();
+
     res.status(200).render('index', {
         courses,
+        categories,
         page_name: 'courses',
         file_name: 'courses'
     });
